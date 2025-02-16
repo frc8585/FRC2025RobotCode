0e751fae4dbd10fe61df4552c315ecb2c591daa1
zhiyouhuang <zhiyouhuang0310@gmail.com>
2025-02-17 00:26:43 +0800

    新增elevator新增elevator

--- src/main/java/frc/robot/elevator.java
+++ src/main/java/frc/robot/elevator.java
@@ -5,7 +5,7 @@ import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.Joystick;
 import com.revrobotics.CANSparkMax;
-import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import com.revrobotics.CANSparkLowLevel.MotorType;
 
 public class elevator extends TimedRobot {
     //private CANSparkMax elevatorMotor = new CANSparkMax(2, MotorType.kBrushless);
@@ -13,8 +13,8 @@ public class elevator extends TimedRobot {
     private RelativeEncoder elevatorEncoder;
     private Joystick joystick = new Joystick(0); 
     private double targetHeight = 0;
-    private final double drumRadius = 0.05; // 捲筒半徑（米）
-    private final int pulleyMultiplier = 2; // 動滑輪倍率
+    private final double drumRadius = 0.05; 
+    private final int pulleyMultiplier = 1; 
 
     @Override
     public void robotInit() {
@@ -34,17 +34,18 @@ public class elevator extends TimedRobot {
 
     @Override
     public void teleopPeriodic() {
-        //double joystickInput = joystick.getRawAxis(5);
-        /*if (Math.abs(joystickInput) > 0.05) { 
+        /*double joystickInput = joystick.getRawAxis(5);
+        if (Math.abs(joystickInput) > 0.05) { 
             targetHeight += joystickInput * 0.1; 
             targetHeight = Math.max(0, Math.min(targetHeight, 1.9)); 
             pidController.setSetpoint(targetHeight);
-        }*/
+        }
 
-        //if (joystick.getRawButtonPressed(1)) targetHeight = 0.46;
-        //if (joystick.getRawButtonPressed(2)) targetHeight = 0.81;
-        //if (joystick.getRawButtonPressed(3)) targetHeight = 1.21;
-        //if (joystick.getRawButtonPressed(4)) targetHeight = 1.83;
+        if (joystick.getRawButtonPressed(1)) targetHeight = 0.95;
+        if (joystick.getRawButtonPressed(2)) targetHeight = 0.46;
+        if (joystick.getRawButtonPressed(3)) targetHeight = 0.81;
+        if (joystick.getRawButtonPressed(4)) targetHeight = 1.21;
+        if (joystick.getRawButtonPressed(5)) targetHeight = 1.83;*/
         pidController.setSetpoint(targetHeight);
 
         double currentHeight = getCurrentHeight();
@@ -54,7 +55,7 @@ public class elevator extends TimedRobot {
     }
 
     private double getCurrentHeight() {
-        double rotations = elevatorEncoder.getPosition(); // 轉換旋轉圈數
-        return (rotations * 2 * Math.PI * drumRadius) / pulleyMultiplier; // 轉換為實際高度
+        double rotations = elevatorEncoder.getPosition(); 
+        return (rotations * 2 * Math.PI * drumRadius) / pulleyMultiplier; 
     }
 }
