86d93c41e1eb7c56daeb75b66795c2b56fa217d7
jjjjj103074 <96659427+jjjjj103074@users.noreply.github.com>
2025-02-16 22:07:56 +0800

    canID設定

--- src/main/java/frc/robot/Swerve.java
+++ src/main/java/frc/robot/Swerve.java
@@ -1,5 +1,22 @@
 package frc.robot;
 
 public class Swerve {
+    // Rotor IDs
+    public static final int kLeftFrontRotorID = 10;
+    public static final int kRightFrontRotorID = 12;
+    public static final int kLeftRearRotorID = 16;
+    public static final int kRightRearRotorID = 14;
+    
+    // Throttle IDs
+    public static final int kLeftFrontThrottleID = 11;
+    public static final int kRightFrontThrottleID = 13;
+    public static final int kLeftRearThrottleID = 17;
+    public static final int kRightRearThrottleID = 15;
+
+    // Rotor Encoder IDs
+    public static final int kLeftFrontRotorEncoderID = 18;
+    public static final int kRightFrontRotorEncoderID = 19;
+    public static final int kLeftRearRotorEncoderID = 21;
+    public static final int kRightRearRotorEncoderID = 20;
     
 }
