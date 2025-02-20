5608c47d225b1648c61f97ae959496c8609a5eff
zhiyouhuang <zhiyouhuang0310@gmail.com>
2025-02-20 22:41:42 +0800

    修改修改

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,12 +4,17 @@
 
 package frc.robot;
 
+import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.util.sendable.SendableRegistry;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.RelativeEncoder;
+import edu.wpi.first.wpilibj.Joystick;
 
 /**
  * The methods in this class are called automatically corresponding to each mode, as described in
@@ -23,6 +28,13 @@ public class Robot extends TimedRobot {
       new DifferentialDrive(m_leftDrive::set, m_rightDrive::set);
   private final XboxController m_controller = new XboxController(0);
   private final Timer m_timer = new Timer();
+  private SparkMax elevatorMotor = new SparkMax(2, MotorType.kBrushless);
+  private PIDController pidController;
+  private RelativeEncoder elevatorEncoder;
+  private double targetHeight = 0;
+  private Joystick joystick = new Joystick(0);
+  private final double drumRadius = 0.05;
+  private final int pulleyMultiplier = 1;
 
   /** Called once at the beginning of the robot program. */
   public Robot() {
@@ -37,6 +49,14 @@ public class Robot extends TimedRobot {
 
   /** This function is run once each time the robot enters autonomous mode. */
   @Override
+  public void robotInit() {
+    elevatorEncoder = elevatorMotor.getEncoder();
+    elevatorEncoder.setPosition(0);
+    pidController = new PIDController(0.1, 0, 0);
+    pidController.setTolerance(0.03);
+  }
+  
+  @Override
   public void autonomousInit() {
     m_timer.restart();
   }
@@ -44,7 +64,7 @@ public class Robot extends TimedRobot {
   /** This function is called periodically during autonomous. */
   @Override
   public void autonomousPeriodic() {
-    // Drive for 2 seconds
+        // Drive for 2 seconds
     if (m_timer.get() < 2.0) {
       // Drive forwards half speed, make sure to turn input squaring off
       m_robotDrive.arcadeDrive(0.5, 0.0, false);
@@ -53,17 +73,54 @@ public class Robot extends TimedRobot {
     }
   }
 
-  /** This function is called once each time the robot enters teleoperated mode. */
+ /** This function is called once each time the robot enters teleoperated mode. */
   @Override
-  public void teleopInit() {}
+  public void teleopInit() {
+    targetHeight = getCurrentHeight();
+    pidController.setSetpoint(targetHeight);
+    pidController.reset();
+  }
 
-  /** This function is called periodically during teleoperated mode. */
+    /** This function is called periodically during teleoperated mode. */
   @Override
   public void teleopPeriodic() {
     m_robotDrive.arcadeDrive(-m_controller.getLeftY(), -m_controller.getRightX());
+
+    double joystickInput = joystick.getRawAxis(5);
+    if (Math.abs(joystickInput) > 0.05) {
+      targetHeight += joystickInput * 0.1;
+      targetHeight = Math.max(0, Math.min(targetHeight, 1.9));
+      pidController.setSetpoint(targetHeight);
+    }
+
+    if (joystick.getRawButtonPressed(1)) 
+      setElevatorHeight(0.46);    
+    if (joystick.getRawButtonPressed(2)) 
+      setElevatorHeight(0.81);
+    if (joystick.getRawButtonPressed(3)) 
+      setElevatorHeight(1.21);
+    if (joystick.getRawButtonPressed(4)) 
+      setElevatorHeight(1.83);
+    if (joystick.getRawButtonPressed(5)) 
+      setElevatorHeight(0.95);
+      
+    double currentHeight = getCurrentHeight();
+    double pidOutput = pidController.calculate(currentHeight);
+    pidOutput = Math.max(-1.0, Math.min(1.0, pidOutput));
+    elevatorMotor.set(pidOutput);
   }
 
   /** This function is called once each time the robot enters test mode. */
+  private double getCurrentHeight() {
+    double rotations = elevatorEncoder.getPosition();
+    return (rotations * 2 * Math.PI * drumRadius) / pulleyMultiplier;
+  }
+
+  private void setElevatorHeight(double height) {
+    targetHeight = height;
+    pidController.setSetpoint(targetHeight);
+  }
+
   @Override
   public void testInit() {}
 
--- src/main/java/frc/robot/elevator.java
+++ src/main/java/frc/robot/elevator.java
@@ -1,61 +1,44 @@
 package frc.robot;
 
 import com.revrobotics.RelativeEncoder;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.SparkMax;
 import edu.wpi.first.math.controller.PIDController;
-import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj.Joystick;
-import com.revrobotics.CANSparkMax;
-import com.revrobotics.CANSparkLowLevel.MotorType;
 
-public class elevator extends TimedRobot {
-    //private CANSparkMax elevatorMotor = new CANSparkMax(2, MotorType.kBrushless);
-    private PIDController pidController;
-    private RelativeEncoder elevatorEncoder;
-    private Joystick joystick = new Joystick(0); 
-    private double targetHeight = 0;
+public class elevator {
+    private final SparkMax motor;
+    private final RelativeEncoder encoder;
+    private final PIDController pidController;
+    private double targetHeight = 0.0;
     private final double drumRadius = 0.05; 
     private final int pulleyMultiplier = 1; 
 
-    @Override
-    public void robotInit() {
-        elevatorMotor.restoreFactoryDefaults();
-        elevatorEncoder = elevatorMotor.getEncoder();
-        elevatorEncoder.setPosition(0);
-        pidController = new PIDController(0.1, 0, 0);
-        pidController.setTolerance(0.03); 
+    public elevator(int motorID) {
+        motor = new SparkMax(motorID, MotorType.kBrushless);
+        encoder = motor.getEncoder();
+        pidController = new PIDController(0.1, 0.0, 0.0);
+        pidController.setTolerance(0.025); 
+        encoder.setPosition(0); 
     }
 
-    @Override
-    public void teleopInit() {
-        targetHeight = getCurrentHeight();
-        pidController.setSetpoint(targetHeight);
-        pidController.reset();
+    public void setHeight(double height) {
+        targetHeight = height;
+        pidController.setSetpoint(height);
     }
 
-    @Override
-    public void teleopPeriodic() {
-        /*double joystickInput = joystick.getRawAxis(5);
-        if (Math.abs(joystickInput) > 0.05) { 
-            targetHeight += joystickInput * 0.1; 
-            targetHeight = Math.max(0, Math.min(targetHeight, 1.9)); 
-            pidController.setSetpoint(targetHeight);
-        }
+    public double getTargetHeight() {
+        return targetHeight;
+    }
 
-        if (joystick.getRawButtonPressed(1)) targetHeight = 0.95;
-        if (joystick.getRawButtonPressed(2)) targetHeight = 0.46;
-        if (joystick.getRawButtonPressed(3)) targetHeight = 0.81;
-        if (joystick.getRawButtonPressed(4)) targetHeight = 1.21;
-        if (joystick.getRawButtonPressed(5)) targetHeight = 1.83;*/
-        pidController.setSetpoint(targetHeight);
-
+    public void update() {
         double currentHeight = getCurrentHeight();
-        double pidOutput = pidController.calculate(currentHeight, targetHeight);
-        pidOutput = Math.max(-1.0, Math.min(1.0, pidOutput)); 
-        elevatorMotor.set(pidOutput);  
+        double output = pidController.calculate(currentHeight);
+        output = Math.max(-1.0, Math.min(1.0, output)); 
+        motor.set(output);
     }
 
-    private double getCurrentHeight() {
-        double rotations = elevatorEncoder.getPosition(); 
-        return (rotations * 2 * Math.PI * drumRadius) / pulleyMultiplier; 
+    public double getCurrentHeight() {
+        double rotations = encoder.getPosition();
+        return (rotations * 2 * Math.PI * drumRadius) / pulleyMultiplier;
     }
 }
